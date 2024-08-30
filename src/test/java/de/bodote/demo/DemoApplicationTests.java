1f48f606277b2c4bc036a1a0913c90e708060dc9
bodote <bodo.te@gmail.com>
2024-08-30 21:26:42 +0200

    rrr

--- src/test/java/de/bodote/demo/DemoApplicationTests.java
+++ src/test/java/de/bodote/demo/DemoApplicationTests.java
@@ -1,5 +1,9 @@
 package de.bodote.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,7 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
